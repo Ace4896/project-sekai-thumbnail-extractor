e9fe82f358d9b624cc4814b2ab792ce4c19d7298
Jon Pacheco <jon.pacheco@tutanota.com>
2023-06-06 19:54:53 +0100

    feat: attempt at extracting thumbnail bounding boxes
    can't get the rectangle drawing to work - not sure what to put in for the colour parameter...

--- src/ts/extractor.ts
+++ src/ts/extractor.ts
@@ -5,22 +5,45 @@ function getMatVectorSize(matVector: cv.MatVector): number {
   return matVector.size() as unknown as number;
 }
 
-function extractFromScreenshot(imgElement: HTMLImageElement) {
-  const imgList = cv.imread(imgElement);
-  const imgBox = extractCharacterBox(imgList);
+function median(numbers: number[]): number {
+  const sorted = Array.from(numbers).sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
 
-  cv.imshow("canvasOutput", imgBox);
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+  }
 
-  // Clear intermediate resources
-  imgList.delete();
-  imgBox.delete();
+  return sorted[middle];
 }
 
-function extractCharacterBox(imgList: cv.Mat): cv.Mat {
-  // The white box surrounding the character thumbnails can be found by searching for the largest external contour
+function extractFromScreenshot(imgElement: HTMLImageElement): cv.Rect[] {
+  const imgList = cv.imread(imgElement);
+
   // Convert to grayscale
   const imgListGray = new cv.Mat();
   cv.cvtColor(imgList, imgListGray, cv.COLOR_BGR2GRAY);
+  
+  // Extract character box
+  const characterBox = extractCharacterBox(imgListGray);
+  const imgBox = imgList.roi(characterBox);
+  const imgBoxGray = imgListGray.roi(characterBox);
+  cv.imshow("canvasOutput", imgBox);
+
+  // Extract thumbnail boxes
+  const thumbnailBoxes = extractThumbnailBoxes(imgBoxGray);
+
+  // Clear intermediate resources
+  imgList.delete();
+  imgListGray.delete();
+  imgBox.delete();
+  imgBoxGray.delete();
+
+  // return thumbnailBoxes;
+  return [];
+}
+
+function extractCharacterBox(imgListGray: cv.Mat): cv.Rect {
+  // The white box surrounding the character thumbnails can be found by searching for the largest external contour
 
   // Threshold to only show pixels that are close to white
   const imgListThresholded = new cv.Mat();
@@ -66,18 +89,74 @@ function extractCharacterBox(imgList: cv.Mat): cv.Mat {
 
   // Get bounding rectangle for approximated contour
   const boundingRect = cv.boundingRect(maxContourApprox);
-  const imgBox = imgList.roi(boundingRect);
 
   // Release intermediate resources
-  imgListGray.delete();
-
   imgListThresholded.delete();
   contoursList.delete();
   hierarchyList.delete();
 
   maxContourApprox.delete();
 
-  return imgBox;
+  return boundingRect;
+}
+
+function extractThumbnailBoxes(imgBoxGray: cv.Mat): cv.Rect[] {
+  // Each of the thumbnails has a rounded square border
+  // We can find them by searching for square-like contours
+
+  // Inversely threshold to show near-white pixels as black
+  const imgBoxThresholded = new cv.Mat();
+  cv.threshold(imgBoxGray, imgBoxThresholded, 250, 255, cv.THRESH_BINARY_INV);
+
+  // Find external contours - this avoids capturing the contents of the thumbnails
+  // This has to be done twice, as the first set of contours are usually disjointed
+  const imgBoxContours = cv.Mat.zeros(imgBoxGray.rows, imgBoxGray.cols, imgBoxGray.type());
+  const contoursBox1 = new cv.MatVector();
+  const hierarchyBox1 = new cv.Mat();
+  cv.findContours(imgBoxThresholded, contoursBox1, hierarchyBox1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+  const contoursCount1 = getMatVectorSize(contoursBox1);
+  for (let i = 0; i < contoursCount1; i++) {
+    const contour = contoursBox1.get(i);
+    const boundingRect = cv.boundingRect(contour);
+
+    // TODO: The colour parameter isn't correct - doesn't seem to accept 255
+    cv.rectangle(
+      imgBoxContours,
+      boundingRect,
+      255,
+      -1
+    );
+  }
+
+  const contoursBox2 = new cv.MatVector();
+  const hierarchyBox2 = new cv.Mat();
+  cv.findContours(imgBoxContours, contoursBox2, hierarchyBox2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+  // After finding second set of contours, get the bounding boxes for each contour
+  // Pick the ones that are square-ish
+  // The most common width comes from the thumbnails; can use ones that are close to median value
+  let thumbnailBoundingBoxes: cv.Rect[] = []
+  const contoursCount2 = getMatVectorSize(contoursBox2);
+  for (let i = 0; i < contoursCount2; i++) {
+    const contour = contoursBox2.get(i);
+    thumbnailBoundingBoxes.push(cv.boundingRect(contour));
+  }
+
+  const lengthThreshold = 0.9 * median(thumbnailBoundingBoxes.map(rect => rect.width));
+  thumbnailBoundingBoxes = thumbnailBoundingBoxes.filter(rect => rect.width > lengthThreshold && rect.height > lengthThreshold);
+
+  // Release intermediate resources
+  imgBoxThresholded.delete();
+  
+  imgBoxContours.delete();
+  contoursBox1.delete();
+  hierarchyBox1.delete();
+
+  contoursBox2.delete();
+  hierarchyBox2.delete();
+
+  return thumbnailBoundingBoxes;
 }
 
 export { extractFromScreenshot };
